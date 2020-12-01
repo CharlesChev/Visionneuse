70ea21b042cd32f6039682685c888dca81d80686
cchevreuil <charles.chevreuil@ign.fr>
2020-12-01 12:04:54 +0100

    ajout cartes libres

--- js/main.js
+++ js/main.js
@@ -4,11 +4,26 @@ Ajouter nom,url, nom de la couche et type de couche (OSM ou WMS)
 
 /*https://geoserver.maps.science/geoserver/OSM/wms?request=GetMap&service=WMS&version1.1.1"*/
 var data = [
-	{	
-		"type":"WMS",
-		"name":"Osm2plan",
-		"url":"https://geoserver.maps.science/geoserver/OSM/wms?request=GetMap&service=WMS&version1.1.1", 
-		"layer":"plan_osm_uk",
+	{
+		"type":"WMTS",
+		"name": "IgnMap", 
+		"url":'https://wxs.ign.fr/pratique/geoportail/wmts', 
+		"layer":"GEOGRAPHICALGRIDSYSTEMS.MAPS",
+		"matrixSet":"PM",
+		"format":"image/jpeg",
+		"projection":"EPSG:3857",
+		"style":"normal",
+		"origin":[-20037508, 20037508],
+		"resolutions":[
+			156543.03392804103,78271.5169640205,39135.75848201024,
+			19567.879241005125,9783.939620502562,4891.969810251281,
+			2445.9849051256406,1222.9924525628203,611.4962262814101,
+			305.74811314070485,152.87405657035254,76.43702828517625,
+			38.218514142588134,19.109257071294063,9.554628535647034,
+			4.777314267823517,2.3886571339117584,1.1943285669558792,
+			0.5971642834779396,0.29858214173896974,0.14929107086948493,
+			0.07464553543474241],
+		"matrixIds":["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"]
 	},
 	{	
 		"type":"OSM",
@@ -17,25 +32,10 @@ var data = [
 		"layer":""
 	},
 	{	
-		"type":"WMTS",
-		"name": "IgnPlanV2", 
-		"url":'https://wxs.ign.fr/choisirgeoportail/geoportail/wmts', 
-		"layer":"GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
-		"matrixSet":"PM",
-		"format":"image/png",
-		"projection":"EPSG:3857",
-		"style":"normal",
-		"origin":[-20037508, 20037508],
-		"resolutions":[
-			156543.03392804103,78271.5169640205,39135.75848201024,
-			19567.879241005125,9783.939620502562,4891.969810251281,
-			2445.9849051256406,1222.9924525628203,611.4962262814101,
-    		305.74811314070485,152.87405657035254,76.43702828517625,
-    		38.218514142588134,19.109257071294063,9.554628535647034,
-    		4.777314267823517,2.3886571339117584,1.1943285669558792,
-    		0.5971642834779396,0.29858214173896974,0.14929107086948493,
-    		0.07464553543474241],
-    	"matrixIds":["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"]
+		"type":"WMS",
+		"name":"Tours",
+		"url":"http://wms.openstreetmap.fr/wms?request=GetMap&service=WMS&version1.1.1", 
+		"layer":"Tours"
 	}
 ];
 
@@ -99,8 +99,8 @@ for (var id in sources){
 /*instanciation de la vue*/
 let view = new ol.View({
 	projection:'EPSG:3857',
-	center:[670594.612659,5980980.58694],
-	zoom:8
+	center:[76895.650455,6007186.052932],
+	zoom:15
 	});
 
 /*carte par default*/
